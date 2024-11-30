8b3a158d237a0f39f0fd452e4d1cafcdc3de6dcb
Markus Kaul <markus.gerrits@gmail.com>
2024-11-30 15:05:45 +0000

    Mundaça no index

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 function Home() {
-  return <div>Welcome to Next.js!</div>;
+  return (
+    <div>
+      <img src="https://upload.wikimedia.org/wikipedia/pt/7/7e/Arte_promocional_de_Enigma_do_Medo.jpg" />
+      <div>
+        <h1>Bora Comprar, Jukka!</h1>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
