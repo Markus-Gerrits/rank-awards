40f20e1025e561025da47419a460567391348949
Markus Kaul <markus.gerrits@gmail.com>
2024-12-11 01:48:28 +0000

    finish /api/v1/status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,25 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum");
-  console.log(result.rows);
-  response.status(200).json({ chave: "O projeto está tomando forma!" });
+  const updateAt = new Date().toISOString();
+  const serverVersion = await database.query("SHOW server_version;");
+  const maxConnections = await database.query("SHOW max_connections;");
+  const databaseName = process.env.POSTGRES_DB;
+  const usedConnections = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  response.status(200).json({
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        version: serverVersion.rows[0].server_version,
+        max_connections: parseInt(maxConnections.rows[0].max_connections),
+        used_connections: usedConnections.rows[0].count,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,42 @@
+const Ajv = require("ajv");
+
 test("GET to api/v1/status should return 200", async () => {
+  const schema = {
+    type: "object",
+    additionalProperties: false,
+    properties: {
+      update_at: { type: "string" },
+      dependencies: {
+        type: "object",
+        additionalProperties: false,
+        properties: {
+          database: {
+            type: "object",
+            additionalProperties: false,
+            properties: {
+              version: { type: "string" },
+              max_connections: { type: "number" },
+              used_connections: { type: "number" },
+            },
+          },
+        },
+      },
+    },
+  };
+  const ajv = new Ajv();
+  const validate = await ajv.compile(schema);
+
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  const valid = validate(responseBody);
+  expect(valid).toBeTruthy();
+
+  expect(responseBody.dependencies.database.version).toBeDefined();
+  expect(responseBody.dependencies.database.version).toMatch(/16.6/);
+  expect(responseBody.dependencies.database.max_connections).toBeDefined();
+  expect(responseBody.dependencies.database.max_connections).toBe(100);
+  expect(responseBody.dependencies.database.used_connections).toBeDefined();
+  expect(responseBody.dependencies.database.used_connections).toBe(1);
 });
